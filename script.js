87b0804e371b377e0f5ea5d5aa691b0b55397191
VetN <122635397+VetN@users.noreply.github.com>
2023-05-22 21:15:50 +0300

    add

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('.btn');
+const btn_1 = document.querySelector('.btn_1');
+const btn_2 = document.querySelector('.btn_2');
+const btn_3 = document.querySelector('.btn_3');
+const btn_4 = document.querySelector('.btn_4');
+btn.addEventListener('click', () => {
+    btn.classList.toggle('btn--turn');
+    
+});
